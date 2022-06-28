07142fdc6c14d5418e4ac866b1213b821e0ba4ba
Airton Araujo Sousa <airton.araujo@rarolabs.com.br>
2022-06-28 15:07:53 -0300

    initial commit

---
+++ lib/features/domain/entities/space_media.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class SpaceMedia extends Equatable {
+  final String description;
+  final String mediaType;
+  final String title;
+  final String mediaUrl;
+
+  SpaceMedia(
+      {required this.description,
+      required this.mediaType,
+      required this.title,
+      required this.mediaUrl});
+
+  @override
+  List<Object?> get props => [description, mediaType, title, mediaUrl];
+}
